ed9cd9c541cd05b25d649a1e9192f68ffdd26d4c
Jagriti Kanwar <kanwarjagritijee21@gmail.com>
2026-06-14 00:40:09 +0530

    Made search UI better

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -72,7 +72,7 @@ function CommandInput({
 }: React.ComponentProps<typeof CommandPrimitive.Input>) {
   return (
     <div data-slot="command-input-wrapper" className="p-1 pb-0">
-      <InputGroup className="h-9 bg-input/30">
+      <InputGroup className="h-9 bg-transparent border-none shadow-none">
         <CommandPrimitive.Input
           data-slot="command-input"
           className={cn(
